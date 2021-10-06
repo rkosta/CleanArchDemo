752146d7b38c06ab91bad4046c0402bc594a5d25
Ricardo Costa <ricardo.costa@create.pt>
2021-10-06 17:53:12 +0100

    Initial commit fot infra data project, updated mvc project for UniversityDBContext

--- CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CleanArch.Infra.Data.Context
 {
-    public class UniversityDBContext
+    public class UniversityDBContext : DbContext
     {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
     }
 }
